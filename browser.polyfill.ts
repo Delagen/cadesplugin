ddc560a42734fca94ac5ae3fd95b327d7c0e7540
Vohmyanin Sergey Vasilevich <delagen@gmail.com>
2019-07-15 13:44:34 +0300

    fix(deps): update dependencies

--- browser.polyfill.ts
+++ browser.polyfill.ts
@@ -1,8 +1,8 @@
 //tslint:disable no-import-side-effect
-import "core-js/es6/promise";
-import "core-js/es6/symbol";
-import "core-js/fn/object/assign";
-import "core-js/fn/object/get-own-property-descriptor";
+import "core-js/features/promise";
+import "core-js/features/symbol";
+import "core-js/features/object/assign";
+import "core-js/features/object/get-own-property-descriptor";
 import "url-search-params-polyfill";
 
 import "./browser";
